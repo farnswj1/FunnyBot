0d340af98b934861ca817ad7b531c98f3b663a31
Justin Farnsworth <farnswj1@tcnj.edu>
2022-11-19 11:26:36 -0500

    updated dependencies and interpreter

--- funnybot/main.py
+++ funnybot/main.py
@@ -1,8 +1,9 @@
-from discord import Client
+from discord import Client, Intents
 import logging
 import random
 import json
 import os
+
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
@@ -17,30 +18,33 @@ class FunnyBot(Client):
         logger.info(f'{self.user} is online!')
 
     async def on_message(self, message):
-        if self.user == message.author:
+        author = message.author
+
+        if self.user == author:
             return
 
-        response = None
-
-        if message.content == '/help':
-            response = '\n'.join((
-                'Here are the commands you can use to interact with me:',
-                '**/joke** --- receive a random joke.',
-                '**/austinpowers** --- receive a random Austin Powers quote.',
-                '**/starwars** --- receive a random quote from Star Wars.'
-            ))
-        elif message.content == '/joke':
-            response = random.choice(self.jokes)
-        elif message.content == '/austinpowers':
-            response = random.choice(self.austin_powers_quotes)
-        elif message.content == '/starwars':
-            response = random.choice(self.star_wars_quotes)
+        match content := message.content:
+            case '/help':
+                response = '\n'.join((
+                    'Here are the commands you can use to interact with me:',
+                    '**/joke** --- receive a random joke.',
+                    '**/austinpowers** --- receive a random Austin Powers quote.',
+                    '**/starwars** --- receive a random quote from Star Wars.'
+                ))
+            case '/joke':
+                response = random.choice(self.jokes)
+            case '/austinpowers':
+                response = random.choice(self.austin_powers_quotes)
+            case '/starwars':
+                response = random.choice(self.star_wars_quotes)
+            case _:
+                response = None
 
         if response:
-            logger.info(f'{message.author}: {message.content}')
+            logger.info(f'{author}: {content}')
             await message.channel.send(response)
 
 
 if __name__ == '__main__':
-    client = FunnyBot()
-    client.run(os.environ.get('DISCORD_TOKEN'))
+    client = FunnyBot(intents=Intents.default())
+    client.run(os.getenv('DISCORD_TOKEN'))
